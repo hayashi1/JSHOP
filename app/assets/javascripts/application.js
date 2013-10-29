ee612839ddabe2bf77b6ae62f89a38cd3c56f0eb
Kotaro Hibi <hibiheion@gmail.com>
2013-10-30 00:32:56 +0900

    スマホ用デザイン

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.accordion
+//= require foundation
 //= require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
--- public/javascript/pref_omiyage.js
+++ public/javascript/pref_omiyage.js
@@ -87,4 +87,4 @@ function getPrefName(result) {
 	});
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);

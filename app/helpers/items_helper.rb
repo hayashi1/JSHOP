44f7f4f226ee626808f6dc88f609e675b401ecbc
Kotaro Hibi <hibiheion@gmail.com>
2013-11-15 00:13:21 +0900

    ランキングHTML作成

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,15 @@
 module ItemsHelper
+  def show_stars(point)
+    stars = '';
+    point_floor = point.floor
+    point_round = point.round
+    point_floor.times do
+      stars << image_tag('star_full.png')
+    end
+    stars << image_tag('star_half.png') if point_floor != point_round
+    (5 - point_round).times do
+      stars << image_tag('star_none.png')
+    end
+    return stars
+  end
 end
